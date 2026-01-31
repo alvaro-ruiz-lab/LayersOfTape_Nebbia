e5056c34d17b83b085f2a0f58de50e8d4f433ef1
alvaroto23 <alvaroruiz.laboral@gmail.com>
2026-01-31 12:17:04 +0100

    Update clases

--- Assets/Scripts/GameData/NPCsData.cs
+++ Assets/Scripts/GameData/NPCsData.cs
@@ -1,84 +1,63 @@
-using UnityEngine;
 
 // 1
-public static class TestiMone
-{
     /// <summary>
+    /// TestiMone
+    /// CHICO
     /// LE ACUSA: Ino Zente
-    /// LE EXCULPA: Dio (estaban follando)
+    /// LE EXCULPA: Dio, estaban follando
     /// CARACTERISTICA: Es un pica flor, pero en vd no
     /// </summary>
-    
-    public static bool acquitted;
-    public static string acquittedReason = "";
-}
 
 
 
 // 2
-public static class SpettAtore
-{
     /// <summary>
-    /// LE ACUSA: Tria Mitto
-    /// LE EXCULPA: Terza Parte
+    /// SpettAtore
+    /// CHICO
+    /// LE ACUSA: Tria Mitto, porque no le deja hablar de sus piramides
+    /// LE EXCULPA: Terza Parte, porque SpettAtore le estaba dando la chapa
     /// CARACTERISTICA: Es un cotilla de cuidao y se cosca de todo
     /// </summary>
-
-    public static bool acquitted;
-    public static string acquittedReason = "";
-}
 
 
 
 // 3
-public static class InoZente
-{
     /// <summary>
+    /// InoZente
+    /// CHICA
     /// LE ACUSA: La carta que lleva a un camino sin salida, ella misma por ser una cagueta sospechosa, Spett Atore que se cosca que era la ultima en entrar
     /// LE EXCULPA: Las pastis para la depresion
     /// CARACTERISTICA: Es to buena gente pero parece mas sospechosa
     /// </summary>
 
-    public static bool acquitted;
-    public static string acquittedReason = "";
-}
-
 
 
 // 4
-public static class TerzaParte
-{
     /// <summary>
-    /// LE ACUSA: Speedwagon
+    /// TerzaParte
+    /// CHICA
+    /// LE ACUSA: Speedwagon, porque le ha robado el sombrero
     /// LE EXCULPA: Su sombrero, no sale sin el
     /// CARACTERISTICA: Speedwagon le ha robado su sombrero, lo sabe aunque no le haya visto
     /// </summary>
 
-    public static bool acquitted;
-    public static string acquittedReason = "";
-}
-
 
 
 // 5
-public static class TriaMitto
-{
     /// <summary>
-    /// LE ACUSA: Si mismo/a
-    /// LE EXCULPA: La carta A que dice que ha pasado tal cosa y no ha podido ser el/ella
+    /// TriaMitto
+    /// CHICO
+    /// LE ACUSA: Si mismo/a, porque porta la piramide de Yu-gi-oh
+    /// LE EXCULPA: La carta A (Tria le cuenta a Signora que se ha jodido la espalda) que dice que ha pasado tal cosa y no ha podido ser el
     /// CARACTERISTICA: Le flipan las piramides
     /// </summary>
-
-    public static bool acquitted;
-    public static string acquittedReason = "";
-}
 
 
 
 // 6
-public static class Dio
-{
     /// <summary>
+    /// Dio
+    /// VAMPIRO
     /// LE ACUSA: Speedwagon, y con razon la vd
     /// LE EXCULPA: Testi Mone, estaban follando
     /// CARACTERISTICA:
@@ -87,23 +66,15 @@ public static class Dio
     ///     - Best... esperabas otro argumento, pero era yo, DIO!
     /// </summary>
 
-    public static bool acquitted;
-    public static string acquittedReason = "";
-}
-
 
 
 // 7
-public static class Speedwagon
-{
     /// <summary>
+    /// Speedwagon
+    /// BESTO WAIFU EVER
     /// LE ACUSA: Terza Parte, por robarle el sombrero
-    /// LE EXCULPA: El sombrero de Terza Parte, se lo estaba robando
+    /// LE EXCULPA: El sombrero de Terza Parte, se lo estaba robando (se descubre robandole 2 veces)
     /// CARACTERISTICA:
     ///     - Besto waifu ever
     ///     - Besto frendo te regala mascarilla a full
     /// </summary>
-
-    public static bool acquitted;
-    public static string acquittedReason = "";
-}
--- Assets/Scripts/GameData/PlayerData.cs
+++ Assets/Scripts/GameData/PlayerData.cs
@@ -1,9 +1,56 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+public class InventoryItem
+{
+    public string itemName;
+    public string acquittedConclusion;
+    public NPC associatedNPC;
+    public bool showOnInventory;
+    public Sprite itemSprite;
+
+    public InventoryItem(string name, string conclusio, NPC npc, bool tangible, Sprite itemImg)
+    {
+        itemName = name;
+        acquittedConclusion = conclusio;
+        associatedNPC = npc;
+        showOnInventory = tangible; // Para decicir si se pone en UI de inventario o no
+        itemSprite = itemImg;
+    }
+}
+
+
+
+public class NPC
+{
+    public string name;
+    public bool enableLoop;
+
+    public NPC(string npcName, bool loop)
+    {
+        name = npcName;
+        enableLoop = loop;
+    }
+}
+
+
+public static class NPCcData
+{
+    public static NPC TestiMone = new NPC(
+        "TestiMone",
+        "Le acusan Ino Zente y le exculpa Dio, estaban follando",
+        false
+    );
+}
+
+
 
 public static class PlayerData
 {
     /// <summary>
     /// NOMBRE: Gio Catore
     /// </summary>
+
+    private static List<InventoryItem> inventory = new List<InventoryItem>();
 
 }
