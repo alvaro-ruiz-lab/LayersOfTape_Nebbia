b5957aed6da573de5afd427a5be028adb5ff39c9
alvaroto23 <alvaroruiz.laboral@gmail.com>
2026-01-31 09:17:55 +0100

    Fixin version error and change data scripts

---
+++ Assets/Scripts/GameData/NPCsData.cs
@@ -0,0 +1,109 @@
+using UnityEngine;
+
+// 1
+public static class TestiMone
+{
+    /// <summary>
+    /// LE ACUSA: Ino Zente
+    /// LE EXCULPA: Dio (estaban follando)
+    /// CARACTERISTICA: Es un pica flor, pero en vd no
+    /// </summary>
+    
+    public static bool acquitted;
+    public static string acquittedReason = "";
+}
+
+
+
+// 2
+public static class SpettAtore
+{
+    /// <summary>
+    /// LE ACUSA: Tria Mitto
+    /// LE EXCULPA: Terza Parte
+    /// CARACTERISTICA: Es un cotilla de cuidao y se cosca de todo
+    /// </summary>
+
+    public static bool acquitted;
+    public static string acquittedReason = "";
+}
+
+
+
+// 3
+public static class InoZente
+{
+    /// <summary>
+    /// LE ACUSA: La carta que lleva a un camino sin salida, ella misma por ser una cagueta sospechosa, Spett Atore que se cosca que era la ultima en entrar
+    /// LE EXCULPA: Las pastis para la depresion
+    /// CARACTERISTICA: Es to buena gente pero parece mas sospechosa
+    /// </summary>
+
+    public static bool acquitted;
+    public static string acquittedReason = "";
+}
+
+
+
+// 4
+public static class TerzaParte
+{
+    /// <summary>
+    /// LE ACUSA: Speedwagon
+    /// LE EXCULPA: Su sombrero, no sale sin el
+    /// CARACTERISTICA: Speedwagon le ha robado su sombrero, lo sabe aunque no le haya visto
+    /// </summary>
+
+    public static bool acquitted;
+    public static string acquittedReason = "";
+}
+
+
+
+// 5
+public static class TriaMitto
+{
+    /// <summary>
+    /// LE ACUSA: Si mismo/a
+    /// LE EXCULPA: La carta A que dice que ha pasado tal cosa y no ha podido ser el/ella
+    /// CARACTERISTICA: Le flipan las piramides
+    /// </summary>
+
+    public static bool acquitted;
+    public static string acquittedReason = "";
+}
+
+
+
+// 6
+public static class Dio
+{
+    /// <summary>
+    /// LE ACUSA: Speedwagon, y con razon la vd
+    /// LE EXCULPA: Testi Mone, estaban follando
+    /// CARACTERISTICA:
+    ///     - Best villano
+    ///     - Best bisexual
+    ///     - Best... esperabas otro argumento, pero era yo, DIO!
+    /// </summary>
+
+    public static bool acquitted;
+    public static string acquittedReason = "";
+}
+
+
+
+// 7
+public static class Speedwagon
+{
+    /// <summary>
+    /// LE ACUSA: Terza Parte, por robarle el sombrero
+    /// LE EXCULPA: El sombrero de Terza Parte, se lo estaba robando
+    /// CARACTERISTICA:
+    ///     - Besto waifu ever
+    ///     - Besto frendo te regala mascarilla a full
+    /// </summary>
+
+    public static bool acquitted;
+    public static string acquittedReason = "";
+}
