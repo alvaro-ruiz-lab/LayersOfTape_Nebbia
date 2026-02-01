53cfbc3f3a610d570f30b782e8b4ead676de3648
alvaroto23 <alvaroruiz.laboral@gmail.com>
2026-02-01 03:15:55 +0100

    Dragdrop scripts

---
+++ Assets/Scripts/UI/DragDrop/Slot.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    // DROP HANDLER---------------------------------------------------
+    public void OnDrop(PointerEventData eventData)
+    {
+        GameObject dropped = eventData.pointerDrag; // Capturar el objeto sobre el que esta el puntero
+
+        if (!dropped.TryGetComponent<DraggableObject>(out var newDraggableObject))
+            return; // Si no es draggable, no hacemos nada
+
+        if (IsDragableObjectNOTABalrog())
+        {
+            newDraggableObject.SetNewParent(transform); // Cambiar el parent para que contenga al objeto
+            return;
+        }
+
+        // Pasar el hijo anterior a las sombras (al parent del nuevo draggable)
+        DraggableObject currentDraggableObject = transform.GetChild(0).GetComponent<DraggableObject>();
+
+        currentDraggableObject.ReturnsToTheShadows(newDraggableObject.ParentAfterDrag);
+
+        newDraggableObject.SetNewParent(transform); // Cambiar el parent para que contenga al nuevo objeto
+    }
+
+
+
+    // HERRAMIENTAS---------------------------------------------------
+    public bool IsDragableObjectNOTABalrog()
+    {
+        if (transform.childCount == 0)
+        {
+            return true;
+        }
+        // print("YOU SHALL NOT PASS!!!!!!!!!!");
+        return false;
+
+//
+//                                                .''.'''
+//                .                             .'   :
+//                \\                          .:    :
+//                 \\                        _:    :       ..----.._
+//                  \\                    .:::.....:::.. .'         ''.
+//                   \\                 .'  #-. .-######'     #        '.
+//                    \\                 '.##' / ' ################       :
+//                     \\                  #####################         :
+//                      \\               ..##.-.#### .''''###'.._        :
+//                       \\             :--:########:            '.    .' :
+//                        \\..__...--.. :--:#######.'   '.         '.     :
+//                        :     :  : : '':'-:'':'::        .         '.  .'
+//                        '---'''..: :    ':    '..'''.      '.        :'
+//                           \\  :: : :     '      ''''''.     '.      .:
+//                            \\ ::  : :     '            '.      '      :
+//                             \\::   : :           ....' ..:       '     '.
+//                              \\::  : :    .....####\\ .~~.:.             :
+//                               \\':.:.:.:'#########.===. ~ |.'-.   . '''.. :
+//                                \\    .'  ########## \ \ _.' '. ' -.       '''.
+//                                :\\  :     ########   \ \      '.  '-.        :
+//                               :  \\'    '   #### :    \ \      :.    '-.      :
+//                              :  .'\\   :'  :     :     \ \       :      '-.    :
+//                             : .'  .\\  '  :      :     :\ \       :        '.   :
+//                             ::   :  \\'  :.      :     : \ \      :          '. :
+//                             ::. :    \\  : :      :    ;  \ \     :           '.:
+//                              : ':    '\\ :  :     :     :  \:\     :        ..'
+//                                 :    ' \\ :        :     ;  \|      :   .'''
+//                                 '.   '  \\:                         :.''
+//                                  .:..... \\:       :            ..''
+//                                 '._____|'.\\......'''''''.:..'''
+//                                            \\
+    }
+}
