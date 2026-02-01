37d7ff00b579d67bd0046a5705c7f806e4fd2b6b
alvaroto23 <alvaroruiz.laboral@gmail.com>
2026-02-01 14:55:13 +0100

    Add visual feedback on oxygen

--- Assets/Scripts/GameControllers/MainUI/OxygenBar.cs
+++ Assets/Scripts/GameControllers/MainUI/OxygenBar.cs
@@ -5,10 +5,15 @@ public class OxygenBar : MonoBehaviour
 {
     [SerializeField] private Image oxygenFillBar;
 
+    private Color originalBlueColor = new Color (86/255f, 157/255f, 217/255f);
+    private Color redColor = new Color (217/255f, 104/255f, 86/255f);
+
 
 
     public void ModifyOxygenAmount(float amount)
     {
         oxygenFillBar.fillAmount = amount;
+
+        oxygenFillBar.color = amount > 0.25f ? originalBlueColor : redColor;
     }
 }
